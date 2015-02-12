4f8aa933835fcfb02a3840147876a288df5ccc2d
Golo Roden <webmaster@goloroden.de>
2015-02-13 00:30:07 +0100

    Updated depedencies.

--- test/recordTests.js
+++ test/recordTests.js
@@ -6,21 +6,21 @@ var record = require('../lib/record.js');
 
 suite('record', function () {
   test('is a function.', function (done) {
-    assert.that(record, is.ofType('function'));
+    assert.that(record).is.ofType('function');
     done();
   });
 
   test('throws an error if fn is missing.', function (done) {
     assert.that(function () {
       record();
-    }, is.throwing('Fn is missing.'));
+    }).is.throwing('Fn is missing.');
     done();
   });
 
   test('throws an error if callback is missing.', function (done) {
     assert.that(function () {
       record(function () {});
-    }, is.throwing('Callback is missing.'));
+    }).is.throwing('Callback is missing.');
     done();
   });
 
@@ -32,8 +32,8 @@ suite('record', function () {
         /*eslint-enable no-console*/
         stop();
       }, function (stdoutText, stderrText) {
-        assert.that(stdoutText, is.equalTo('foo\n'));
-        assert.that(stderrText, is.equalTo(''));
+        assert.that(stdoutText).is.equalTo('foo\n');
+        assert.that(stderrText).is.equalTo('');
         done();
       });
     });
@@ -46,8 +46,8 @@ suite('record', function () {
         /*eslint-enable no-console*/
         stop();
       }, function (stdoutText, stderrText) {
-        assert.that(stdoutText, is.equalTo('foo\nbar\n'));
-        assert.that(stderrText, is.equalTo(''));
+        assert.that(stdoutText).is.equalTo('foo\nbar\n');
+        assert.that(stderrText).is.equalTo('');
         done();
       });
     });
@@ -61,8 +61,8 @@ suite('record', function () {
         /*eslint-enable no-console*/
         stop();
       }, function (stdoutText, stderrText) {
-        assert.that(stdoutText, is.equalTo(''));
-        assert.that(stderrText, is.equalTo('foo\n'));
+        assert.that(stdoutText).is.equalTo('');
+        assert.that(stderrText).is.equalTo('foo\n');
         done();
       });
     });
@@ -75,8 +75,8 @@ suite('record', function () {
         /*eslint-enable no-console*/
         stop();
       }, function (stdoutText, stderrText) {
-        assert.that(stdoutText, is.equalTo(''));
-        assert.that(stderrText, is.equalTo('foo\nbar\n'));
+        assert.that(stdoutText).is.equalTo('');
+        assert.that(stderrText).is.equalTo('foo\nbar\n');
         done();
       });
     });
